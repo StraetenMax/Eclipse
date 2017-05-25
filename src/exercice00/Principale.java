c3d9422569cfedef2cb90ecacd8fda293fb7fcc5
StraetenMax <maxstraeten@yahoo.com>
2017-05-25 15:50:25 +0200

    mettre en place le debug

--- src/exercice00/Principale.java
+++ src/exercice00/Principale.java
@@ -1,8 +1,20 @@
 package exercice00;
 
 public class Principale {
+	
+	public static void uneAutreMethode(){
+		System.out.println("Comment va le monde");
+	}
+	
+	
 	public static void main(String[] args){
 		System.out.println("Bonjour le monde");
+		int var = 4;
+		uneAutreMethode();
+		
+		uneAutreMethode();
+		System.out.println(var);
+		
 	}
 		
 	
